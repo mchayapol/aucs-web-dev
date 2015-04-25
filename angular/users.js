a0ab9dae9b5ba55712b9c61f7ddefd02d5557235
mchayapol <mchayapol@gmail.com>
2015-04-25 20:15:55 +0700

    add existing code

---
+++ angular/users.js
@@ -0,0 +1,7 @@
+[
+    {id:1, fName:'Chayapol'  , lName:"Moemeng" },
+    {id:2, fName:'Kim'   , lName:"Pim" },
+    {id:3, fName:'Jack'  , lName:"Jones" },
+    {id:4, fName:'John'  , lName:"Doe" },
+    {id:5, fName:'Peter' , lName:"Pan" }
+]
