b308b7ac93607ed30f6ada150e326f6897017c2b
Chayapol Moemeng <mchayapol@gmail.com>
2015-10-27 10:20:33 +0700

    Add Read operation Master-detail page

---
+++ php/mysql/db.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "movies";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
--- php/mysql/movie-comment.php
+++ php/mysql/movie-comment.php
@@ -6,21 +6,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+if(! isset($_GET['mid'])) {
+	die("Invalid parameter<br><a href='index.php'>Go Back</a>");
+}
 
-$sql = "SELECT * FROM movie";	// wild card
+$mid = $_GET['mid'];
+
+$mid = mysqli_real_escape_string($conn, $mid);
+
+$sql = "SELECT * FROM comment WHERE mid = $mid";	// wild card
 $result = $conn->query($sql);	// cursor
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<a href='movie-comment.php?mid=".$row['id']."'><b>" 
-			. $row["name"]. "</b></a><br>" 
-			. $row["description"]. "<br>";
+        echo "<b>". $row["name"]. ": </b>" 
+			. $row["comment"]. "<br>";
     }
 } else {
     echo "0 results";
 }
 $conn->close();
-
-
 ?>
+<a href="/">Go Back</a>
